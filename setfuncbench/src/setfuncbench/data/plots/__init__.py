eeebb1d524d9322e4e6924dcb6993149ac69a5dd
st-ep <sdt2344@my.utexas.edu>
2026-02-07 23:32:20 -0600

    Add __init__.py for plotting module in SetFuncBench

---
+++ setfuncbench/src/setfuncbench/data/plots/__init__.py
@@ -0,0 +1,21 @@
+from setfuncbench.data.plots.plot_dataset1_shared_quadratic import (
+    load_batch_from_pt,
+    plot_dataset1_shared_quadratic_batch,
+)
+from setfuncbench.data.plots.plot_dataset2_mixture_curvatures import (
+    plot_dataset2_mixture_curvatures_batch,
+)
+from setfuncbench.data.plots.plot_dataset3_hidden_pairing import (
+    plot_dataset3_hidden_pairing_batch,
+)
+from setfuncbench.data.plots.plot_dataset4_pointer_chasing import (
+    plot_dataset4_pointer_chasing_batch,
+)
+
+__all__ = [
+    "load_batch_from_pt",
+    "plot_dataset1_shared_quadratic_batch",
+    "plot_dataset2_mixture_curvatures_batch",
+    "plot_dataset3_hidden_pairing_batch",
+    "plot_dataset4_pointer_chasing_batch",
+]
